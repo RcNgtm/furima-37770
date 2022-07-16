29e7291b1c866477d663b014f0c8883d7e46901f
RcNgtm <rika.nagatomo243@gmail.com>
2022-07-16 17:59:08 +0900

    ActiveHashの実装
    ・データ用モデルの作成
    ・出品用ビューに反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :sales_status, :shipping_fee_status, :scheduled_delivery, :prefecture
+
 end
