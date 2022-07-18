c1bca127059bcfb57237fea8c3516f703a1af450
RcNgtm <rika.nagatomo243@gmail.com>
2022-07-18 21:39:31 +0900

    商品情報の保存
    ・Active storageのインストール
    ・Mini Magicのインストール
    ・image_processingのインストール
    ・createアクションの記述
    ・バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,22 @@
 class Item < ApplicationRecord
-
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :sales_status, :shipping_fee_status, :scheduled_delivery, :prefecture
+  belongs_to :category 
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :scheduled_delivery
+  belongs_to :prefecture
+  has_one_attached :image
 
+  validates :image , presence: true
+  validates :item_name , presence: true
+  validates :items_explain , presence: true
+  validates :category_id , numericality: { other_than: 1 ,message: "can't be blank "}
+  validates :sales_status_id , numericality: { other_than: 1 ,message: "can't be blank" }
+  validates :shipping_fee_status_id , numericality: { other_than: 1 ,message: "can't be blank" }
+  validates :prefecture_id , numericality: { other_than: 1 ,message: "can't be blank" }
+  validates :scheduled_delivery_id , numericality: { other_than: 1 ,message: "can't be blank" }
+  validates :user_id , presence: true
+  validates :item_price , presence: true , numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } ,
+            format: { with: /\A[0-9]+\z/ }
+  
 end
