b7d2506af0d250a998478acbbae6250eb009327b
RcNgtm <rika.nagatomo243@gmail.com>
2022-07-23 17:00:49 +0900

    商品一覧表示機能修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
     @item = Item.includes(:user).order("created_at DESC")
   end
@@ -21,5 +23,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:item_name, :items_explain, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :item_price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
